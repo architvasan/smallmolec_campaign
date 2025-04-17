adfea4cea042e81a256e46e6c1821a9bf404a2de
architvasan <avasan@anl.gov>
2025-04-17 14:28:57 -0700

    added code for training model

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='small molecule campaign',
+    version='0.1.0',
+    packages=['smallmolec_campaign'
+              ],
+    url='https://github.com/architvasan/smallmolec_campaign',
+    license='MIT',
+    author='Archit Vasan',
+    author_email='avasan@anl.gov',
+    description='Python package for running large small molecule capaign',
+)
